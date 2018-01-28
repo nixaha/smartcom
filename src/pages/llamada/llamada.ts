acb0339469e17c33065c7d018a3711afabcb2042
Nixaha <nixaha30saucedo@gmail.com>
2018-01-27 18:59:14 -0600

    camara

--- src/pages/llamada/llamada.ts
+++ src/pages/llamada/llamada.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-llamada',
   templateUrl: 'llamada.html',
 })
 export class LlamadaPage {
+  home: any = HomePage; 
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
